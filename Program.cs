c89b9cd5ffed3656a0351d54a69728b8efc99d4f
Gabe <gabrielemartinez@ucundinamarca.edu.co>
2025-03-16 21:07:32 -0500

    build: added nuggets and build pipeline

--- Program.cs
+++ Program.cs
@@ -1,25 +1,41 @@
+using ArandanoIRT_Backend.Infrastructure.Config;
+using ArandanoIRT_Backend.Infrastructure.Data;
+using ArandanoIRT_Backend.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options 
+    => options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString")));
+
+DependencyInjection.ConfigureServices(builder.Services, builder.Configuration);
+LoggingConfig.ConfigureServices(builder.Services, builder.Configuration);
+builder.Services.AddHostedService<LogCleanupService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseReDoc(c =>
+    {
+        c.Path = "/redoc";
+        c.DocumentPath = "/swagger/v1/swagger.json";
+    });
+}
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
