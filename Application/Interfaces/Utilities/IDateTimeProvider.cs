c0b1c1874fbe32bdfb3350fddb9d3cf9abafe7cf
Gabe <gabrielemartinez@ucundinamarca.edu.co>
2025-04-13 21:21:40 -0500

    feat: UTC time utility

---
+++ Application/Interfaces/Utilities/IDateTimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using ArandanoIRT_Backend.Domain.ValueObjects; 
+
+namespace ArandanoIRT_Backend.Application.Interfaces.Utilities
+{
+    /// <summary>
+    /// Provides the current time, abstracting away direct DateTime usage for testability.
+    /// </summary>
+    public interface IDateTimeProvider
+    {
+        /// <summary>
+        /// Gets the current UTC DateTime.
+        /// </summary>
+        DateTime GetUtcNow();
+    }
+}
