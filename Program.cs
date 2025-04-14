bd25d389e0762408098c7f6dbc854fc19ee83228
Gabe <gabrielemartinez@ucundinamarca.edu.co>
2025-04-13 21:26:36 -0500

    build: updated configuration file

--- Program.cs
+++ Program.cs
@@ -1,20 +1,46 @@
 using ArandanoIRT_Backend.Infrastructure.Config;
 using ArandanoIRT_Backend.Infrastructure.Data;
+using ArandanoIRT_Backend.Infrastructure.Persistence.Interceptors;
 using ArandanoIRT_Backend.Infrastructure.Services;
+using FluentValidation.AspNetCore;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using ArandanoIRT_Backend.UI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options 
-    => options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionString")));
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<AuditSaveChangesInterceptor>();
+
+var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
+builder.Services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+{
+    options.UseNpgsql(connectionString);
+    options.AddInterceptors(serviceProvider.GetRequiredService<AuditSaveChangesInterceptor>());
+});
 
 DependencyInjection.ConfigureServices(builder.Services, builder.Configuration);
 LoggingConfig.ConfigureServices(builder.Services, builder.Configuration);
 builder.Services.AddHostedService<LogCleanupService>();
+RateLimitConfig.ConfigureRateLimiting(builder.Services, builder.Configuration);
 
+builder.Host.UseSerilog();
+builder.Services.AddMemoryCache();
+builder.Services.AddCustomCors();
+builder.Services.AddJwtAuthentication(builder.Configuration);
+//builder.Services.AddValidatorsFromAssemblyContaining<ActivityRequestDtoValidator>();
+builder.Services.ConfigureResponseCompression();
+builder.Services.ConfigureJsonOptions();
+
+builder.Services.AddHttpClient();
 builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwagger();
+builder.Services.AddOutputCache();
 
 var app = builder.Build();
 
@@ -28,14 +54,23 @@ if (app.Environment.IsDevelopment())
         c.DocumentPath = "/swagger/v1/swagger.json";
     });
 }
+app.UseHttpsRedirection();
+app.UseExceptionHandling();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseHttpsRedirection();
+app.UseCors("OnlyFrontend");
+app.UseSerilogRequestLogging();
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseRateLimiter();
+app.UseSanitization();
+app.UseResponseCompression();
+app.UseOutputCache();
 app.MapControllers();
+app.Run();
 
 app.Run();
