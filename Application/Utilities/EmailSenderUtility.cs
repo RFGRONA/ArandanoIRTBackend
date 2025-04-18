25f85f7664ed60074467d5fe26e77fce0dc2c0c7
Gabe <gabrielemartinez@ucundinamarca.edu.co>
2025-04-18 00:44:22 -0500

    fix: variable is uninitialized

--- Application/Utilities/EmailSenderUtility.cs
+++ Application/Utilities/EmailSenderUtility.cs
@@ -42,8 +42,7 @@ namespace ArandanoIRT_Backend.Application.Utilities
             }
 
             // Use int.TryParse for safe parsing
-            int portValue;
-            if (!int.TryParse(portString, out portValue))
+            if (!int.TryParse(portString, out int portValue))
             {
                 // Throw exception if TryParse fails (invalid format)
                 throw new InvalidOperationException($"Configuration value for 'Smtp:Port' ('{portString}') is not a valid integer format.");
@@ -158,7 +157,7 @@ namespace ArandanoIRT_Backend.Application.Utilities
                 }
 
                 // Decode Base64 (consider TryParse if base64 source is unreliable)
-                byte[] fileBytes;
+                byte[] fileBytes = [];
                 try
                 {
                     fileBytes = Convert.FromBase64String(fileBase64);
